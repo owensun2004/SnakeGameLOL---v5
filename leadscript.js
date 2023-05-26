5be6b236f334df10bc1d041ecd160cdb1b5446e0
owensun2004 <58713459+owensun2004@users.noreply.github.com>
2023-05-26 05:12:27 -0400

    Add files via upload

--- leadscript.js
+++ leadscript.js
@@ -1,9 +1,16 @@
 const ul = document.querySelector('ul');
 const input = document.getElementById('item');
+var ptitle=document.getElementById('smttit');
+var btn=document.getElementById('addbut');
+var checker=0;
 let itemsArray = localStorage.getItem('items') ?
-
-
 JSON.parse(localStorage.getItem('items')) : [];
+
+for(let i=0;i<itemsArray.length;i++){
+    itemsArray[i][0]=parseInt(itemsArray[i][0]);
+}
+itemsArray.sort(sortFunction);
+
 addTask(itemsArray);
 //console.log(itemsArray);
 //itemsArray.forEach(addTask);
@@ -22,19 +29,52 @@ function addTask(arr){
     ul.innerHTML="";
   for(let i=0;i<arr.length;i++){
     const li = document.createElement('li');
-    li.textContent = arr[i][1] + " "+ arr[i][0];
+    li.textContent = arr[i][1];
     ul.appendChild(li);
+    const li2 = document.createElement('li');
+    li2.textContent = arr[i][0];
+    ul.appendChild(li2);
   }
 }
 
 function add(){
   //itemsArray.push(input.value);
-  if(checkName(input.value, itemsArray)==0){
-    itemsArray.push([getCookie("gamescore"), input.value]);
+  if(checker==0){
+    let a= checkName(input.value, itemsArray);
+    if(a==0){
+      //NOTE1 maybe consider parsing getCookie to int??
+      if(itemsArray.length==10 && parseInt(getCookie("gamescore"))<itemsArray[9][0]){
+        //case 1: max limit and new player score is less than top 10
+        alerting();
+        return;
+      }else if(itemsArray.length==10){
+        //case 2: max limit shown with new player with high score
+        itemsArray.pop();
+        itemsArray.push([getCookie("gamescore"), input.value]);
+      }else{
+        //case 3: new player with high score and no max limit
+        itemsArray.push([getCookie("gamescore"), input.value]);
+      }
+      alerting2(2);
+      return;
+    }else if(a==1){
+      alerting2(1);
+      return;
+    }else{
+      alerting2(1.5)
+      return;
+    }
+    
+  }else{
+    checker=0;
+    btn.innerHTML="Add Item";
+    ptitle.innerHTML="Submit your score here";
+    input.classList.remove("hide");
   }
   itemsArray.sort(sortFunction);
   localStorage.setItem('items', JSON.stringify(itemsArray));
   addTask(itemsArray);
+
   input.value = '';
   document.cookie="lpop=0;";
   //sortList();
@@ -43,11 +83,49 @@ function add(){
   
 }
 
+function XclosePopUp(){
+  if(checker==1){
+    add();
+    return;
+  }
+  input.value = '';
+  document.cookie="lpop=0;";
+  //sortList();
+  todo.classList.remove("open-popup");
+    overlay.classList.remove('active');
+}
+
+function alerting(){
+  btn.innerHTML="Okay";
+  ptitle.innerHTML="Sorry, your score is too low, pls play again";
+  input.classList.add("hide");
+  checker=1;
+}
+
+function alerting2(num){
+  btn.innerHTML="Okay";
+  if(num==1){
+    ptitle.innerHTML="Hi "+ input.value+", your score has been updated with your new record!";
+  }else if(num==1.5){
+    ptitle.innerHTML="Hi "+ input.value+", your score will not be updated since your new score is lower than your original";
+  }else if(num==2){
+    ptitle.innerHTML="Welcome to Snake Game "+ input.value+"! Your score has been added to the leaderboard!";
+  }
+  input.classList.add("hide");
+  checker=1;
+}
+
 function checkName(pname, arr){
     for(let i=0;i<arr.length;i++){
         if(pname==arr[i][1]){
+            //NOTE1 same here
+          if(arr[i][0]<parseInt(getCookie("gamescore"))){
             arr[i][0]=getCookie("gamescore");
             return 1;
+          }else{
+            return 2;
+          }
+            
         }
     }
     return 0;
